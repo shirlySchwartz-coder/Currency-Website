db83b0bf18af4c7cc17eff859707dfa60b469f31
shirlySchwartz-coder <s0577611175@gmail.com>
2019-12-06 07:49:06 +0200

    parallax

--- api.js
+++ api.js
@@ -4,38 +4,60 @@ var default_page = "panel1";
 
 
 $(function() {
-  allcoins = getCurrencys();
-  console.log("allCoins : " + allcoins);
-  //
+   
   $("[id*='panel']").hide();
   $("#" + default_page).show();
+  homePage(allcoins);
+    
 
   $(".panel-button").on("click", function() {
     var panelId = $(this).data("panelid");
+    console.log("panelId: " +panelId)
     $("[id*='panel']").hide();
     $("#" + panelId).show();
-    
+
+    switch(panelId) {
+      case 'panel3':
+        // code block
+        alert("About!");
+        break;
+      case 'panel2':
+        // code block
+        alert("Live!");
+        break;
+      default:
+        // code block
+        alert("Home!");
+        homePage(allcoins);
+    }
+
   });
-  //
-  // $("#c1").show().siblings().hide("slow");
-  if ($("#panel1") && allcoins > 0) {
-    homePage(allcoins);
-  }
+  
+
 });
 
-function loadHomePage() {
-  $(".nav-link").addEventListner.on("click", function(id) {
-    alert(id);
-  });
-  var back_img = $('<img src="./media/DSC_0915c.jpg"/>');
-}
+
+
 function homePage(cardData) {
-  $("#cont1").append(back_img);
-  alert("Loading");
-  //console.log("allCoins : " + allCoins);
-  if (allCoins.length > 0) {
-    createCards(allCoins);
+  var div_home=$('#cont1');
+  var back_img = $(`<div class="parallax" id="back_img">`);
+  var hr= $('<hr>');
+  div_home.append(back_img).append(hr);
+
+  if (cardData.length==0){
+    var progresBar= $(
+    `<div class="progress">
+      <div class="progress-bar" style="width:70%">
+    </div>
+  </div>`);
+    alert("Loading");
+    
+     getCurrencys(allcoins);
   }
+ else{
+  
+   // createCards(allcoins);
+ }
 }
 
 function createCards(cardData) {
@@ -50,8 +72,8 @@ function createCards(cardData) {
     var body = $('<div class="card-body"></div>');
     var first_row = $('<div class="row"></div>');
     var card_switch = $(
-      `<label class="switch"><input type="checkbox" checked><span class="slider round"></span></label>`
-    );
+    `<div class="myslider"><label class="switch"><input type="checkbox" checked><span class="slider round"></span></label></div>`
+     );
     var symbol = $(
       `<div class="card-title"><h4> ${cardData[i].symbol}<h4></div>`
     );
@@ -70,22 +92,38 @@ function createCards(cardData) {
   }
 }
 
-function getCurrencys() {
+function getCurrencys(allcoins) {
   var coins = [];
   $.ajax({
     type: "GET",
     url: "https://api.coingecko.com/api/v3/coins/list",
-    success: function(coins) {
-      console.log("1" + coins);
-      if (coins.length > 0) {
-        allcoins = coins;
+    success: function(allcoins) {
+      //console.log("1" + coins);
+      if (allcoins.length > 0) {
+        allcoins = allcoins;
         createCards(allcoins);
       }
     }
   });
   //return allcoins;
 }
-
+function getCoin() {
+  let searchcoin={};
+  let search = $('#search').val();
+  if (search.length < 3) {
+      alert("Search Phrase too Short...");
+      return;
+  }
+  $.ajax({
+    type: "GET",
+    url: `https://api.coingecko.com/api/v3/coins/${search}`,
+    success: function(searchcoin) {
+      console.log( searchcoin);
+      
+    }
+  });
+ 
+}
 function printCurArray(array) {
   console.log("Array:" + array);
   for (let i = 0; i < 100; i++) {
