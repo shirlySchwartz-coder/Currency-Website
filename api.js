80c8b7e61f68ce19ffc5f651f986f3c3a8234c03
shirlySchwartz-coder <58229972+shirlySchwartz-coder@users.noreply.github.com>
2019-12-16 14:00:30 +0200

    not working selction
    the function is selecting all elements not only the ones how checked

--- api.js
+++ api.js
@@ -1,14 +1,15 @@
 var allcoins = [];
 var coins = [];
 var default_page = "panel1";
-var selected_coins=[];
+var selected_coins = [];
 
 $(function() {
-  $( document ).ajaxStart(function() {
-    $( "#loading" ).show();
+  $(document).ajaxStart(function() {
+    $("#loading").show();
   });
   $("[id*='panel']").hide();
   $("#" + default_page).show();
+
   homePage(allcoins);
 
   $(".panel-button").on("click", function() {
@@ -36,23 +37,29 @@ $(function() {
 });
 
 function homePage(cardData) {
-  let sitetitel="Currency WebSite";
+  var selected_coins = null;
+  
+  let sitetitel = "Currency WebSite";
   var div_home = $("#cont1");
   var back_img = $(`<div class="parallax" id="back_img"></div>`);
-  var page_titel = $(`<div class="img_titel"><span class="border">${sitetitel}</span> </div>`);
+  var page_titel = $(
+    `<div class="img_titel"><span class="border">${sitetitel}</span> </div>`
+  );
   div_home.append(back_img).append(page_titel);
 
   if (cardData.length == 0) {
     getCurrencys(cardData);
   }
+  
 }
 
 function createCards(cardData) {
   // console.log("createCards");
-  console.log(cardData);  
+  //console.log(cardData);
   for (let i = 0; i < 90; i++) {
-      createCard(cardData[i]);
+    createCard(cardData[i]);
   }
+  
 }
 
 function getCurrencys(allcoins) {
@@ -62,53 +69,82 @@ function getCurrencys(allcoins) {
     type: "GET",
     url: "https://api.coingecko.com/api/v3/coins/list",
     success: function(allcoins) {
-     
       $(".cover").hide();
       if (allcoins.length > 0) {
         createCards(allcoins);
       }
     }
   });
+ 
 }
 
-function createCard(coin){
+function createCard(coin) {
+ // var card_id= coin.id;
+  //console.log(card_id);
   var div_card = $("#card-wrapper");
   var card = $(
-    `<div class='col-md-3 outer-card card' style="width: 18rem;" key = ${coin.id}></div>`
+    `<div class='col-md-3 outer-card card' style="width: 18rem;" cardkey = ${coin.id} id=${coin.id}></div>`
   );
+  //console.log(coin.id);
 
   var body = $('<div class="card-body"></div>');
   var first_row = $('<div class="row"></div>');
-  
+
   var symbol = $(
     `<div class="card-title" style="width: 50%;"><h4> ${coin.symbol}<h4></div>`
   );
-  var card_switch = $(
-    `<div class="myslider" style="width: 50%;"><label class="switch"><input type="checkbox" ><span class="slider round"></span></label></div>`
-  );
+  
+  var div_switch=$('<div class="myslider" style="width: 50%;"></div>');
+  var label_switch=$('<label class="switch" ></label>');
+  var input_switch =$('<input type="checkbox" key="${coin.id}" id="switchSlider" >');
+  var span_switch=$('<span class="slider round"></span>');
+
+  
+  $('input').change(function(){
+    if(this.checked==true ){
+      addToSelected(this,coin.id,coin.symbol);
+    }
+  });
+
+  $(label_switch).append(input_switch);
+  $(label_switch).append(span_switch);
+
+  $(div_switch).append(label_switch);
+  
+
   var name = $(`<div class="card-text">${coin.name}</div>`);
-  var infobtn = $(` <button class="btn btn-primary" id="info_btn">More Info</button>`);
-  infobtn.on('click',function(coin){
-    info_coin={};
-    $.ajax({
-      type: "GET",
-      url: `https://api.coingecko.com/api/v3/coins/${coin.name}`,
-      success: function(info_coin) {
-        console.log(info_coin);
-        moreInfoCreate(info_coin);  
-      }
-    });    
+
+  var infobtn = $(
+    ` <button class="btn btn-primary collapsible"  id="info_btn">More Info</button>`
+  );
+  var infoWrapper = $(`<div class="content"></div>`);
+  infobtn.on("click", function() {
+    var time = Date.now();
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      console.log("hide");
+    } else {
+      content.style.display = "block";
+      console.log("show");
+      console.log(time);
+      getInfoOnCoin(infoWrapper, coin.id, time);
+    }
   });
 
   $(first_row)
     .append(symbol)
-    .append(card_switch);
+    .append(div_switch);
   $(body)
     .append(first_row)
     .append(name)
-    .append(infobtn);
+    .append(infobtn)
+    .append(infoWrapper);
   $(card).append(body);
   div_card.append(card);
+
+  
 }
 
 function getCoin() {
@@ -125,24 +161,66 @@ function getCoin() {
       console.log(searchcoin);
       $("#card-wrapper").html("");
       console.log(searchcoin);
-      createCard(searchcoin); 
-      
+      createCard(searchcoin);
     }
   });
 }
 
 function printCurArray(array) {
-  console.log( array);
+  console.log(array);
   console.log("startin to print array");
   for (let i = 0; i < 100; i++) {
     let coin = array[i];
   }
 }
 
-function moreInfoCreate(coin) {
-  var more_info_div= $(`<a href="#demo" data-toggle="collapse" style="text-align:center">Coin Info</a>`);
-  var coin_info=$(`<div id="demo" class="collapse">${coin.name}</div>`);
+function getInfoOnCoin(infoWrapper, coinId, now) {
+  $("#infoWrapper").html("");
+  $.ajax({
+    type: "GET",
+    url: `https://api.coingecko.com/api/v3/coins/${coinId}`,
+    success: function(infoCoin) {
+      console.log(infoCoin);
+      var coin_img = $(
+        `<img src="${infoCoin.image.large}" id="coin_img" srcset="">`
+      );
+      var coin_vs_USD = $(
+        `<div><label>USD: </label>${infoCoin.market_data.current_price.usd}"$" </div>`
+      );
+      var coin_vs_EUR = $(
+        `<div><label>EUR: </label>${infoCoin.market_data.current_price.eur} </div>`
+      );
+      var coin_vs_ILS = $(
+        `<div><label>ILS: </label>${infoCoin.market_data.current_price.ils} </div>`
+      );
 
-  card.insertAfter(more_info_div).insertAfter(coin_info);
+      infoWrapper
+        .append(coin_img)
+        .append(coin_vs_USD)
+        .append(coin_vs_EUR)
+        .append(coin_vs_ILS);
+      var now = new Date();
+      console.log(now.toUTCString());
+    }
+  });
 }
 
+function addToSelected(coin,coinid,coinsymbol){
+  console.log(coin,coinid,coinsymbol);
+}
+
+/*function myfunc() {
+    var checked_coin= $(this);
+    console.log(this);
+   alert(checked_coin);
+    
+ 
+}*/
+/*  $(".card").on("change",'input',  function() {
+    console.log(e.id);
+    selected_coins.push(id);
+    
+  });
+  //selected_coins.push(selcoin.id);
+  //console.log(selected_coins);
+}*/
